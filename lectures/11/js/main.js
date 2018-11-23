efb0d215ba6b21a44a41316ce029bdd5d4c08304
Faisal Jawad <faisaljawad1@gmail.com>
2018-11-23 23:46:30 +0500

    Line-through added in Step 5 and only Edit button added but not its functionality.

--- lectures/11/js/main.js
+++ lectures/11/js/main.js
@@ -3,20 +3,27 @@
 //============================================
 
 
-/*var todos = [];
+/*
+var todos = [];
 function add() {
     var task = document.getElementById("task").value;
     todos.push(task);
     document.getElementById('todos').innerText = todos;
-}*/
+}
+*/
 
+/*
+Answer: Todo array is created in which all the new tasks will be pushed dynamically.
+then we are displaying index by index on the bottom area (comma separated)
+*/
 
 //============================================
 //-----------  STEP - 2 ----------------------
 //============================================
 
 
-/*var todos = [];
+/*
+var todos = [];
 function add() {
     var task = document.getElementById('task').value;
     todos.push(task);
@@ -34,13 +41,15 @@ function show() {
         ul.appendChild(li);
     }
     document.getElementById('todos').appendChild(ul);
-}*/
+}
+*/
 
+/*Answer: Unordered list element is made to add the elements now. Each elements is entered in the start of list.
+Append mode is used which will over right the new and old one again which will cause redundancy in the tasks list.*/
 
 //============================================
 //-----------  STEP - 3 ----------------------
 //============================================
-
 
 /*
 function getTodos() {
@@ -83,8 +92,12 @@ function show() {
     }
     document.getElementById('todos').appendChild(ul);
 }
-show();
-*/
+show();*/
+
+/*
+Answer: In this code, we are adding new tasks but it is not creating any redundancy along.
+We are fetching previously entered data from the LocalStorage object. JSON.Parse is helping to retreive data from the server end.
+Delete button is also added but it is not functional yet. */
 
 
 
@@ -164,6 +177,11 @@ function isDone(e) {
 }
 show();*/
 
+/*Answer: In this code, data is being stored on local storage like the previous one.
+    Delete Button is properly functioning now and inline styling is also added.
+    Trim() is removing the whitespaces from both sides of the string.
+    Tasks are added in order list now.*/
+
 
 
 
@@ -181,7 +199,88 @@ HINT: use text-decoration:line-through; property of CSS
 
 2- Enable Editing todos in text field to update text
 
-
-
 */
 
+function getTodos() {
+    var todos = [];
+    var todos_str = localStorage.getItem('todo');
+    if(todos_str !== null)
+        todos = JSON.parse(todos_str);
+    return todos;
+}
+
+function add() {
+    var task = document.getElementById('task').value;
+    if(task.trim() == ''){
+        document.getElementById('message').style.display = 'block';
+        return false;
+    } else {
+        document.getElementById('message').style.display = 'none';
+    }
+    var todos = getTodos();
+    todos.push({task: task, isDone: false});
+    document.getElementById('task').value = '';
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+    return false;
+}
+
+function remove() {
+    var id = this.getAttribute('id');
+    var todos = getTodos();
+    todos.splice(id,1);
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+    return false;
+}
+
+function edit(){
+    var id = this.getAttribute('id');
+    
+}
+
+function strike(i){
+    document.getElementById("text"+i).style='text-decoration:line-through';
+}
+
+function show() {
+    document.getElementById('todos').innerText = '';
+    var todos = getTodos();
+    var ul = document.createElement('ul');
+    ul.classList.add("list-group");
+    for(var i=0; i<todos.length; i++){
+        var li = document.createElement('li');
+        li.innerHTML  = '<li onclick="strike('+i+')" id="text'+i+'">' + todos[i].task + '</li>' +
+            '<button class="btn btn-primary" id="' + i + '">' +
+            '<span class="d-none d-sm-inline"> Edit </span> </button>'+
+            '<button class="btn btn-danger" id="' + i + '">' +
+            '<i class="fa fa-trash-o"></i> ' +
+            '<span class="d-none d-sm-inline"> Delete </span> </button>';
+        li.classList.add("list-group-item");
+
+
+        if(todos[i].isDone)
+            li.classList.add("done");
+        ul.appendChild(li);
+    }
+    document.getElementById('todos').appendChild(ul);
+    var buttons = document.getElementsByClassName('btn-danger');
+    for(var i=0; i<buttons.length; i++){
+        buttons[i].addEventListener('click',remove);
+    }
+}
+
+function isDone(e) {
+    var todos = getTodos();
+    if(todos[e.target.id].isDone) {
+        e.target.classList.add('done');
+        todos[e.target.id].isDone = false;
+    }
+    else{
+        e.target.classList.remove('done');
+        todos[e.target.id].isDone = true;
+    }
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+}
+show();
